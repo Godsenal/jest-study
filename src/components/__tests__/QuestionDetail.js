5bc3ae013129e839cf4792aefb9664da9d977ee3
Godsenal <tmqps78@gmail.com>
2018-08-06 20:24:08 +0900

    Add QuestionDetail test

---
+++ src/components/__tests__/QuestionDetail.js
@@ -0,0 +1,38 @@
+import { mapStateToProps, QuestionDetailDisplay } from '../QuestionDetail';
+import renderer from 'react-test-renderer';
+import React from 'react';
+
+describe('The Question Detail Component', () => {
+    describe('The Container Element', () => {
+        describe('mapStateToProps', () => {
+            it ('should map the state to props correctly', () => {
+                const sampleQuestion = {
+                    question_id: 42,
+                    body: "space is big"
+                };
+                const appState = {
+                    questions: [sampleQuestion],
+                };
+                const ownProps = {
+                    question_id: 42,
+                };
+                const componentState = mapStateToProps(appState, ownProps);
+                console.log(componentState);
+                expect(componentState).toEqual(sampleQuestion);
+            });
+        });
+    });
+    describe('The display element', () => {
+        it ('Should not regress', () => {
+            const tree = renderer.create(
+                <QuestionDetailDisplay
+                    title="The meaning of lift"
+                    body="4"
+                    answer_count={0}
+                    tags={['hitchhiking']}
+                />
+            );
+            expect(tree.json).toMatchSnapshot();
+        })
+    })
+});
